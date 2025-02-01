4cefd7a4c62299932db106cc7efc3a1e7d062e09
Blewiy <m263708@usna.edu>
2025-02-01 14:46:44 -0500

    with comments

--- problemsets/ps3/part1.py
+++ problemsets/ps3/part1.py
@@ -9,11 +9,41 @@ import sys
 #Sources 
 
 #https://www.w3schools.com/python/ref_string_isnumeric.asp
-array []
+array = []
+companies = {} #k = company number , v = num ppl in company
 user_input = sys.stdin.readline()
 while(user_input.split(" ")[0].isnumeric()):
     array.append(user_input)
+    if user_input.split(" ")[0] in companies:
+        val = companies[user_input.split(" ")[0]] + 1
+        companies.update({user_input.split(" ")[0]:val})
+    else:
+        companies.update({user_input.split(" ")[0]:1})
     user_input = sys.stdin.readline()
-
+    
 print(array)
+print(companies)
+num_in_company_counted = 0
+c = 1 #which company we're using first
+for i in range(len(array)):
+    print(str(c) + " ___ " + str(i) + " ____ " + str(len(array)))
+    if array[i].split(" ")[0] != str(c):
+        k = i + 1
+        while array[k].split(" ")[0] != str(c):
+            k += 1
 
+        #swappppppp
+        temp = array[i]
+        array[i] = array[k]
+        array[k] = temp
+        print(array)
+    num_in_company_counted+= 1
+
+    if num_in_company_counted == companies[str(c)]:
+        num_in_company_counted = 0
+        c += 1
+    print()
+    print(array)
+    print()
+print(array)
+print(companies)
